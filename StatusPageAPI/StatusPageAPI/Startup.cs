a5ded30368860f0595bd5ba7f5a7db97241058d3
Serenity <d.palbanese@gmail.com>
2020-08-01 21:31:42 +0200

    Setup all the default things i need to start :)

--- StatusPageAPI/StatusPageAPI/Startup.cs
+++ StatusPageAPI/StatusPageAPI/Startup.cs
@@ -1,17 +1,30 @@
+using System.Net;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StatusPageAPI.Extensions;
 
 namespace StatusPageAPI
 {
     public class Startup
     {
+        
+        // For later use
+        public void ConfigureDevelopmentServices(IServiceCollection services)
+        {
+            this.ConfigureServices(services);
+        }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddRouting(op => op.LowercaseUrls = true);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -21,12 +34,31 @@ namespace StatusPageAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler(b =>
+                {
+                    b.Run(async context =>
+                    {
+                        context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+
+                        var err = context.Features.Get<IExceptionHandlerFeature>();
+                        if (err != null)
+                        {
+                            context.Response.AddApplicationError(err.Error.Message);
+                            await context.Response.WriteAsync(err.Error.Message);
+                        }
+                    });
+                });
+            }
+            
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+                endpoints.MapControllers();
             });
         }
     }
