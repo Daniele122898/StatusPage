a03270cbd47bf1007b5438e96a66061ee74fb4dc
Serenity <d.palbanese@gmail.com>
2020-08-11 16:00:40 +0200

    Need more and better declaration. idk what i was smoking

--- StatusPageAPI/StatusPageAPI/Models/EntityDeclaration.cs
+++ StatusPageAPI/StatusPageAPI/Models/EntityDeclaration.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StatusPageAPI.Models
 {
     public class EntityDeclaration
     {
         public string Identifier { get; set; }
+        public string Description { get; set; }
         public Uri HealthEndpoint { get; set; }
+
+        public List<EntityDeclaration> SubEntities { get; set; }
+
         public bool Enabled { get; set; } = true;
+        public bool IsCategory { get; set; } = false;
     }
 }
