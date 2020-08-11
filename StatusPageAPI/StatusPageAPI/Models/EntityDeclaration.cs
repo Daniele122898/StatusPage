dc490de51f002adb568b1654c2fe10063b9063be
Serenity <d.palbanese@gmail.com>
2020-08-11 12:36:49 +0200

    Added identifier override so we can name the service differently if we wish

--- StatusPageAPI/StatusPageAPI/Models/EntityDeclaration.cs
+++ StatusPageAPI/StatusPageAPI/Models/EntityDeclaration.cs
@@ -5,6 +5,7 @@ namespace StatusPageAPI.Models
     public class EntityDeclaration
     {
         public Uri HealthEndpoint { get; set; }
+        public string IdentifierOverride { get; set; }
         public bool Enabled { get; set; } = true;
     }
 }
